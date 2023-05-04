c990f9cfab01c1647e5d2eb948f555871009398a
Ross Thomson <rthomson412@gmail.com>
2023-05-04 17:20:53 -0400

    cart menu import

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,11 @@ import { useEffect } from "react";
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import Home from "./scenes/home/Home";
 import Navbar from "./scenes/global/Navbar";
+import Footer from "./scenes/global/Footer";
 import ItemDetails from "./scenes/itemDetails/ItemDetails";
+import CartMenu from "./scenes/global/CartMenu";
 import Checkout from "./scenes/checkout/Checkout";
 import Confirmation from "./scenes/checkout/Confirmation";
-
 
 const ScrollToTop = () => {
   const { pathname } = useLocation();
@@ -20,17 +21,19 @@ const ScrollToTop = () => {
 function App() {
   return (
     <div className="app">
-    <BrowserRouter>
-      <Navbar />
-      <ScrollToTop />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="item/:itemId" element={<ItemDetails />} />
-        <Route path="checkout" element={<Checkout />} />
-        <Route path="checkout/success" element={<Confirmation />} />
-      </Routes>
-    </BrowserRouter>
-  </div>
+      <BrowserRouter>
+        <Navbar />
+        <ScrollToTop />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="item/:itemId" element={<ItemDetails />} />
+          <Route path="checkout" element={<Checkout />} />
+          <Route path="checkout/success" element={<Confirmation />} />
+        </Routes>
+        <CartMenu />
+        <Footer />
+      </BrowserRouter>
+    </div>
   );
 }
 
