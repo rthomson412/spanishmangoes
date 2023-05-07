bef983f177239d563f93684b5b50886be917ab2d
Ross Thomson <rthomson412@gmail.com>
2023-05-07 16:32:49 -0400

    shopping list

--- client/src/scenes/home/Home.jsx
+++ client/src/scenes/home/Home.jsx
@@ -1,5 +1,14 @@
-const Home = () => {
-  return <div>Home</div>;
-};
+
+import MainCarousel from "./MainCarousel";
+import ShoppingList from "./ShoppingList";
+
+function Home() {
+  return (
+    <div className="home">
+      <MainCarousel />
+      <ShoppingList />
+    </div>
+  );
+}
 
 export default Home;
---
+++ client/src/scenes/home/ShoppingList.jsx
@@ -0,0 +1,5 @@
+const ShoppingList = () => {
+
+}
+
+export default ShoppingList;
