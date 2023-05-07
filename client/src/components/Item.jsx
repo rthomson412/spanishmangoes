ec7ff5d8edb2de14c8432658d90bbb79e11855f9
Ross Thomson <rthomson412@gmail.com>
2023-05-07 14:24:16 -0400

    Item hooks/ imports

---
+++ client/src/components/Item.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { IconButton, Box, Typography, useTheme, Button } from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+import RemoveIcon from "@mui/icons-material/Remove";
+import { shades } from "../theme";
+import { addToCart } from "../state";
+import { useNavigate } from "react-router-dom";
+
+const Item = ({ item, width }) => {
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const [count, setCount] = useState(1);
+    const [isHovered, setIsHovered] = useState(false);
+    const {
+      palette: { neutral },
+    } = useTheme();
+
+export default Item;
