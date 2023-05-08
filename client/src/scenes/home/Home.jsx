a8939cf0e8e26ceec8feee694dd2d1126a7dc9f8
Ross Thomson <rthomson412@gmail.com>
2023-05-08 18:17:23 -0400

    subscribe

--- client/src/scenes/home/Home.jsx
+++ client/src/scenes/home/Home.jsx
@@ -1,12 +1,14 @@
 
 import MainCarousel from "./MainCarousel";
 import ShoppingList from "./ShoppingList";
+import Subscribe from "./Subscribe";
 
 function Home() {
   return (
     <div className="home">
       <MainCarousel />
       <ShoppingList />
+      <Subscribe />
     </div>
   );
 }
